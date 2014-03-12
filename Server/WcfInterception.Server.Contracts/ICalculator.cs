9646825d2ec326f2928ac6c5c2896825739e5b6c
Sergey Psekha <pss@ciklum.com>
2014-03-12 16:50:50 +0200

    added solution;

---
+++ Server/WcfInterception.Server.Contracts/ICalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WcfInterception.Server.Contracts
+{
+    public interface ICalculator
+    {
+    }
+}
