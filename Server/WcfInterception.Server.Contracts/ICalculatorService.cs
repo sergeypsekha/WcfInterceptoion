d9cca5fefab3598f65939f557f55e73e24565e4a
Sergey Psekha <pss@ciklum.com>
2014-03-12 16:58:15 +0200

    added implementation;

---
+++ Server/WcfInterception.Server.Contracts/ICalculatorService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfInterception.Server.Contracts
+{
+    [ServiceContract(Name = "CalculatorService", Namespace = "http://schemas.WcfInterception/2014/03/calculator")]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        int Add(int a, int b);
+    }
+}
---
+++ Server/WcfInterception.Server.Services/CalculatorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using WcfInterception.Server.Contracts;
+
+namespace WcfInterception.Server.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
